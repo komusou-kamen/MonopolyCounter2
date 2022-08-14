d0a0420a836117200f7c8992c50d68cc770d71a4
komusou-kamen <77836529+komusou-kamen@users.noreply.github.com>
2022-08-14 07:23:07 +0200

    Initial Commit

---
+++ MonopolyCounter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonopolyCounter2
+//
+//  Created by Komusou on 2022/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MonopolyCounter2/MonopolyCounter2App.swift
@@ -0,0 +1,17 @@
+//
+//  MonopolyCounter2App.swift
+//  MonopolyCounter2
+//
+//  Created by Komusou on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MonopolyCounter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
